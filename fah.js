584d5b4762f3a34d9d46377c9be56370e4055e53
Саша <@gmail.com>
2026-03-30 10:33:26 +0300

    Добавлена функция addLetterGrade и анализ успеваемости

---
+++ fah.js
@@ -0,0 +1,31 @@
+const grades = [
+    { name: "Макар", score: 85 },
+    { name: "Денис", score: 92 },
+    { name: "Анна", score: 78 },
+    { name: "Даша", score: 88 },
+    { name: "Студент_X", score: 45 }
+];
+
+function addLetterGrade(data) {
+    return data.map(student => {
+        const { score } = student;
+        let letter;
+
+        if (score >= 90) {
+            letter = 'A';
+        } else if (score >= 75) {
+            letter = 'B';
+        } else {
+            letter = 'C';
+        }
+
+        return { ...student, letter };
+    });
+}
+
+const gradesWithLetters = addLetterGrade(grades);
+
+console.log("Анализ успеваемости:");
+gradesWithLetters.forEach(student => {
+    console.log(`${student.name}: ${student.score} баллов — ${student.letter}`);
+});
