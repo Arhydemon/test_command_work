7978a75bf89f340d4290d7b0c0606486395ecfea
Arhydemon <1692328@gmail.com>
2026-03-30 10:21:49 +0300

    changes

--- lab2/main.js
+++ lab2/main.js
@@ -1,3 +1,14 @@
+import { calculateAverage } from "./functions/calculateAverage.js";
+import { findTopStudent } from "./functions/findTopStudent.js";
+import { filterFailed } from "./functions/filterFailed.js";
+import { addLetterGrade } from "./functions/addLetterGrade.js";
+import { getTotalScore } from "./functions/getTotalScore.js";
+import { countPassed } from "./functions/countPassed.js";
+import { getStudentNames } from "./functions/getStudentNames.js";
+import { sortByScore } from "./functions/sortByScore.js";
+import { findByName } from "./functions/findByName.js";
+
+// Общие данные (не менять)
 const grades = [
     { name: "Макар", score: 85 },
     { name: "Денис", score: 92 },
@@ -6,7 +17,14 @@ const grades = [
     { name: "Студент_X", score: 45 }
 ];
 
-// функции будут сюда добавляться
+// Итоговый вывод (собирает Макар)
 
-// итоговый вывод
-// (пока пусто)
+console.log("Средний балл:", calculateAverage(grades)); // Денис
+console.log("Лучший студент:", findTopStudent(grades)); // Сережа
+console.log("Должники:", filterFailed(grades, 60)); // Дима Л
+console.log("С оценками:", addLetterGrade(grades)); // Никита
+console.log("Сумма баллов:", getTotalScore(grades)); // Кирилл
+console.log("Сдавшие:", countPassed(grades, 60)); // Костя
+console.log("Имена:", getStudentNames(grades)); // Дима В
+console.log("Сортировка:", sortByScore(grades)); // Артур
+console.log("Поиск:", findByName(grades, "Макар")); // Денис Г
