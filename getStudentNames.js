2361a85b22e7cea5d3660ebcec94908980e8486a
Arhydemon <147500264+Arhydemon@users.noreply.github.com>
2026-03-30 17:55:47 +0300

    Refactor getStudentNames function to export
    Убрал тестовые данные и console.log, добавил export. Функция должна быть чистой и использоваться через main.js.

--- getStudentNames.js
+++ getStudentNames.js
@@ -1,18 +1,9 @@
-const students = [
-  { name: "Макар", role: "teamlead", exp: 5 },
-  { name: "Денис", role: "programmer", exp: 4 },
-  { name: "Анна", role: "programmer", exp: 2 },
-  { name: "Даша", role: "designer", exp: 1 }
-];
-  
+export function getStudentNames(data) {
+    let names = [];
 
-function getStudentNames(data) {
-  let names = [];
-  for (let i = 0; i < data.length; i++) {
-    names.push(data[i].name);
-  }
-  return names;
+    for (let i = 0; i < data.length; i++) {
+        names.push(data[i].name);
+    }
+
+    return names;
 }
-
-console.log("\nиу:");
-console.log(getStudentNames(students))
