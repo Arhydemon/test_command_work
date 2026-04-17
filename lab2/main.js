507fb67ccbf23ee5c478d0bf4451c891b1cf834d
Arhydemon <147500264+Arhydemon@users.noreply.github.com>
2026-04-17 13:12:15 +0300

    Update main.js

--- lab2/main.js
+++ lab2/main.js
@@ -1,13 +1,7 @@
-import { calculateAverage } from "./calculateAverage.js";
-import { findTopStudent } from "./findTopStudent.js";
-import { filterFailed } from "./filterFailed.js"; // готово
-import { addLetterGrade } from "./addLetterGrade.js";
-import { getTotalScore } from "./getTotalScore.js";
-import { countPassed } from "./countPassed.js"; // готово
-import { getStudentNames } from "./getStudentNames.js"; // готово
-import { sortByScore } from "./sortByScore.js";
-import { findByName } from "./findByName.js";
-import { getAverageByGrade } from "./getAverageByGrade.js"; // готово
+import { findTopStudent } from "./findTopStudent.js"; // Сережа ГОТОВО
+import { filterFailed } from "./filterFailed.js"; // Дима Лубченко ГОТОВО
+import { countPassed } from "./countPassed.js"; // Костя ГОТОВО
+import { getStudentNames } from "./getStudentNames.js"; /// Дима Вологин ГОТОВО
 
 // Общие данные (не менять)
 const grades = [
@@ -20,13 +14,7 @@ const grades = [
 
 // Итоговый вывод
 
-console.log("Средний балл:", calculateAverage(grades)); // Денис Сундуков
-console.log("Лучший студент:", findTopStudent(grades)); // Сережа не вайб кодер
-console.log("Должники:", filterFailed(grades, 60)); // Дима Лубченко
-console.log("С оценками:", addLetterGrade(grades)); // Никита
-console.log("Сумма баллов:", getTotalScore(grades)); // Кирилл
-console.log("Сдавшие:", countPassed(grades, 60)); // Костя
-console.log("Имена:", getStudentNames(grades)); // Дима Вологин
-console.log("Сортировка:", sortByScore(grades)); // Артур
-console.log("Поиск:", findByName(grades, "Макар")); // Денис Г
-console.log("Средний балл по оценкам:", getAverageByGrade(addLetterGrade(grades))); // Дима Кудинов
+console.log("Лучший студент:", findTopStudent(grades)); // Сережа не вайб кодер ГОТОВО
+console.log("Должники:", filterFailed(grades, 60)); // Дима Лубченко ГОТОВО
+console.log("Сдавшие:", countPassed(grades, 60)); // Костя ГОТОВО
+console.log("Имена:", getStudentNames(grades)); // Дима Вологин ГОТОВО
